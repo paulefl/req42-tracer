ba4a5a6ecdd897e6af15b2e4c7294ad351b5f229
dev-paul-fleischmann <dev@paul-fleischmann.com>
2026-05-31 16:07:20 +0200

    Refs #162: User-Guide Sub-Seiten als HTML bauen + Links auf .html statt .adoc

--- docToolchainConfig.groovy
+++ docToolchainConfig.groovy
@@ -2,9 +2,14 @@ outputPath = System.getenv('DTC_OUTPUT_PATH') ?: 'build/docs'
 inputPath  = 'project/req42-tracer/docs'
 
 inputFiles = [
-    [file: 'user-guide/pdf.adoc',   formats: ['pdf']],
-    [file: 'user-guide/index.adoc', formats: ['html']],
-    [file: 'arc42/arc42.adoc',      formats: ['html']],
+    [file: 'user-guide/pdf.adoc',                     formats: ['pdf']],
+    [file: 'user-guide/index.adoc',                   formats: ['html']],
+    [file: 'user-guide/getting-started.adoc',         formats: ['html']],
+    [file: 'user-guide/command-reference.adoc',       formats: ['html']],
+    [file: 'user-guide/configuration-reference.adoc', formats: ['html']],
+    [file: 'user-guide/block-attribute-reference.adoc', formats: ['html']],
+    [file: 'user-guide/workflow-guide.adoc',          formats: ['html']],
+    [file: 'arc42/arc42.adoc',                        formats: ['html']],
 ]
 
 imageDirs = ["${inputPath}/images"]
